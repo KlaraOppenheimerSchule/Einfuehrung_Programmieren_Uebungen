3148abaa778e5b6597be44eeab26af5c3cd6c7d3
Christoph Zobel <zobel@klara-oppenheimer-schule.de>
2023-08-11 10:53:28 +0200

    Including C#

---
+++ Modul OOP Einführung/UebungMathe/Math.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UebungMathe
+{
+    internal class Math
+    {
+        public int sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int square(int a)
+        {
+            return a * a;
+        }
+
+        public bool checkPos(int a)
+        {
+            return a > 0; //returns true if positive
+        }
+
+        public int fac(int a)
+        {
+            int result = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                result = result*i;
+            }
+            return result;
+        }
+
+        public int facrec(int a)
+        {
+            if (a == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return a * facrec(a - 1);
+            }
+        }
+
+    }
+}
---
+++ Modul OOP Einführung/UebungMathe/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UebungMathe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mathobj = new Math();
+            Console.WriteLine(mathobj.sum(3, 4));
+            Console.WriteLine(mathobj.square(3));
+            Console.WriteLine(mathobj.fac(3));
+            Console.WriteLine(mathobj.facrec(3));
+            Console.ReadLine();
+        }
+    }
+}
