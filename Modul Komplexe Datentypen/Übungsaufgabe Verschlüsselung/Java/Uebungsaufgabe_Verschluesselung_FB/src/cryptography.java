293496fa66dec050876148c1e3c9c761ecc98ef2
fboehme <florian.boehme@ssi-schaefer.com>
2024-01-24 11:18:14 +0100

    file reading, encryption

--- Modul Komplexe Datentypen/Übungsaufgabe Verschlüsselung/Java/Uebungsaufgabe_Verschluesselung_FB/src/cryptography.java
+++ Modul Komplexe Datentypen/Übungsaufgabe Verschlüsselung/Java/Uebungsaufgabe_Verschluesselung_FB/src/cryptography.java
@@ -5,15 +5,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.function.Predicate;
+
+import static java.lang.System.exit;
 
 public class cryptography {
     // current working directory is the repo root directory
     private static final String ASSET_PATH = "Modul Komplexe Datentypen\\Übungsaufgabe Verschlüsselung\\Java\\Uebungsaufgabe_Verschluesselung_FB\\assets";
     private static final Predicate<Path> TXT_FILE_FILTER = path -> path.toString().endsWith(".txt");
+    private enum STATE_TAGS {ENCRYPT, DECRYPT}
+    private static STATE_TAGS currentState;
 
     public static void main(String[] args) throws IOException {
         /*String currentDirectory = System.getProperty("user.dir");
@@ -28,50 +30,109 @@ public class cryptography {
                 It will make a copy of all .txt files inside the assets folder and apply your chosen method to it.
                 The program will handle files with their name ending with "_encrypted" as encrypted files. Everything else
                 as unencrypted.
-                Do you wish to encrypt or unencrypt? (e/u)                
+                Do you wish to encrypt or decrypt? (e/u)                
                 """);
         System.out.println("#".repeat(60));
 
+        // fetch the input and fetch the files accordingly
         String input = scanner.next();
 
+        Map<String, String> fileContents = null;
+
         if (input.equalsIgnoreCase("e")) {
-            letsEncrypt();
-        } else if (input.equalsIgnoreCase("u")) {
-            letsDecrypt();
+            currentState = STATE_TAGS.ENCRYPT;
+            fileContents = readTxtFiles(fetchEncryptFiles());
+        } else if (input.equalsIgnoreCase("d")) {
+            currentState = STATE_TAGS.DECRYPT;
+            fileContents = readTxtFiles(fetchDecryptFiles());
         } else {
             System.out.println("#".repeat(60));
             System.out.print("""
                 Your input does not match the intended format. Better luck next time!              
                 """);
             System.out.println("#".repeat(60));
+            exit(666);
         }
+
+        //Talk to user again about the encryption key
+        System.out.println("#".repeat(60));
+        System.out.print("""
+                Great! You have chosen to {{state}}!
+                Please insert you encryption key. Currently the service accepts only single integer values
+                ranging from 32-255.
+                """.replace("{{state}}", String.valueOf(currentState)));
+        System.out.println("#".repeat(60));
+
+        // fetch the input from the user
+        int cryptKey = 0;
+        try {
+            cryptKey = Integer.parseInt(scanner.next());
+        } catch (NumberFormatException e) {
+            System.out.println("You picked the wrong format! Better luck next time!");
+            exit(666);
+        }
+
+        //negate the key if we want to decrypt for easier traversing over the ascii table
+        if (currentState == STATE_TAGS.DECRYPT) {
+            cryptKey = -cryptKey;
+        }
+
+        // iterate over the files to perform encryption and writeback
+        for (Map.Entry<String, String> entry : fileContents.entrySet()) {
+            switch (currentState) {
+                case ENCRYPT:
+                    letsEncrypt(entry.getKey(), entry.getValue(), cryptKey);
+                case DECRYPT:
+                    letsDecrypt(entry.getValue(), entry.getValue(), cryptKey);
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    private static void letsEncrypt(String fileName, String fileContent, int cryptKey) {
+        String enctyptedContent = "";
+        //TODO Arbeite hier weiter
+        for (char character : fileContent.toCharArray()) {
+            //sloppy casting and string concatenation but should work
+            int rep = (int) character;
+            rep = rep + cryptKey;
+            enctyptedContent += Character.toChars(rep)[0];
+        }
+    }
+
+    private static void letsDecrypt(String fileName, String fileContent, int cryptKey) {
     }
 
     /**
      * Fetches the files inside the asset directory. Filters the file list for file names ending with _encrypted.
+     *
+     * @return  List<Path>
      * @throws IOException
      */
-    private static void letsEncrypt() throws IOException {
-        List<Path> files = new ArrayList<>();
+    private static List<Path> fetchEncryptFiles() throws IOException {
+        List<Path> files;
 
         files = fetchTxtFiles(ASSET_PATH);
         files.removeIf(path -> path.toString().endsWith("_encrypted.txt"));
 
-        List<String> fileContents = new ArrayList<>();
-
-        fileContents = readTxtFiles(files);
-        //TODO HIer weiter
+        return files;
     }
 
     /**
      * Fetches the files inside the asset directory. Filters the file list for file names ending with _encrypted.
+     *
+     * @return List<Path>
      * @throws IOException
      */
-    private static void letsDecrypt() throws IOException {
-        List<Path> files = new ArrayList<>();
+    private static List<Path> fetchDecryptFiles() throws IOException {
+        List<Path> files;
+
         files = fetchTxtFiles(ASSET_PATH);
         files.removeIf(path -> !path.toString().endsWith("_encrypted.txt"));
-        readTxtFiles(files);
+
+        return files;
     }
 
     /**
@@ -103,10 +164,10 @@ public class cryptography {
      * @return List<String>
      * @throws IOException
      */
-    public static List<String> readTxtFiles(List<Path> txtFiles) throws IOException {
-        List<String> contents = new ArrayList<>();
+    public static Map<String, String> readTxtFiles(List<Path> txtFiles) throws IOException {
+        Map<String, String> contents = new HashMap<>();
         for (Path file : txtFiles) {
-            contents.addAll(Files.readAllLines(file));
+            contents.put(file.getFileName().toString(), Files.readString(file));
         }
         return contents;
     }
