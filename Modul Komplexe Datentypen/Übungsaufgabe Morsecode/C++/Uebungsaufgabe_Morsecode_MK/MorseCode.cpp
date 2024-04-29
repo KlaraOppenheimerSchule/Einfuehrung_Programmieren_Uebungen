1dd3f4a2062ea2646bef12771f0b19323fe2cab9
IdiotamSpielen <105610621+IdiotamSpielen@users.noreply.github.com>
2024-04-29 11:18:20 +0200

    Task MorseCode

--- Modul Komplexe Datentypen/Übungsaufgabe Morsecode/C++/Uebungsaufgabe_Morsecode_MK/MorseCode.cpp
+++ Modul Komplexe Datentypen/Übungsaufgabe Morsecode/C++/Uebungsaufgabe_Morsecode_MK/MorseCode.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <map>
+#include <algorithm>
 using namespace std;
 
 //Primary:
 //[x] Entwickle ein Programm, das Buchstaben in Morsecode übersetzt
 //Speichern Sie hierzu einfach einen Buchstaben in einer Variablen und lassen Sie diesen sodann übersetzten.
 //Secondary:
-//[ ] Übersetzung ganzer wörter
+//[x] Übersetzung ganzer wörter
 //[ ] Übersetzung ganzer sätze
 
+string translate(const string& str, const map<char, string>& morse_codes){
+    string translated;
+    for (char c : str){
+        if (morse_codes.count(c)){
+            translated.append(morse_codes.at(c));
+        }
+    }
+    return translated;
+}
 
+string CAPSLOCK(const string& str, map<char, string>& morse_codes){  
+    string stringCaps;
+    for (char c : str){
+        c = toupper(c);
+        stringCaps.push_back(c);
+    }
+    return stringCaps;
+}
 
 int main()
 {
@@ -22,18 +40,17 @@ int main()
         {'1', ".----"}, {'2', "..---"}, {'3', "...--"}, {'4', "....-"}, {'5', "....."},
         {'6', "-...."}, {'7', "--..."}, {'8', "---.."}, {'9', "----."}, {'0', "-----"}
     };
-    char letter;
-    cout << "Welchen Buchstaben wollen Sie Uebersetzen?" << endl;
-    cin >> letter;
-    letter = toupper(letter);
 
-    if (morse_codes.count(letter))
-    {
-        cout << "Der Buchstabe: " << letter << endl
-        << "wird uebersetzt als: " << morse_codes.at(letter) << endl;
-    }
+    string toTranslate;
+    cout << "Was wollen Sie Uebersetzen?" << endl;
+    cin >> toTranslate;
 
+    cout << "Die Eingabe:" << toTranslate << endl;
 
+    toTranslate = CAPSLOCK(toTranslate, morse_codes);
+    string translated = translate(toTranslate, morse_codes);
+
+    cout << "wird uebersetzt als:" << translated << endl;
 
     return 0;
 }
