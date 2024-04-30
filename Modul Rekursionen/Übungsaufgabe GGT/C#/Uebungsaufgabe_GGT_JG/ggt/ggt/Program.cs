820a54157a850a0f7d3f69d24c494b90c62486da
Julia Gütling <J.Guetling1@schueler.klara-oppenheimer-schule.de>
2024-04-30 12:41:10 +0200

    Uebungen_JG_GGT+Fibonacci

---
+++ Modul Rekursionen/Übungsaufgabe GGT/C#/Uebungsaufgabe_GGT_JG/ggt/ggt/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Gib die erste Zahl an.");
+int eingabe1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Gib die zweite Zahl an.");
+int eingabe2 = int.Parse(Console.ReadLine());
+
+if (eingabe1 > eingabe2)
+{
+    Console.WriteLine($"Der größte gemeinsame Teiler der beiden Zahlen {eingabe1} und {eingabe2} ist {getGGT(eingabe2, eingabe1)}");
+}
+else
+{
+    Console.WriteLine($"Der größte gemeinsame Teiler der beiden Zahlen {eingabe1} und {eingabe2} ist {getGGT(eingabe1, eingabe2)}");
+}
+
+int getGGT(int zahl1, int zahl2)
+{
+    if (zahl1 == zahl2 || zahl2 == 0)
+    {
+        return zahl1;
+    }
+    else
+    {
+        return getGGT(zahl2, zahl1 % zahl2);
+    }
+}
