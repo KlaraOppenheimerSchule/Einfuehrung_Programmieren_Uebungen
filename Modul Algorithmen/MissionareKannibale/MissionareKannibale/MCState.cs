3d7097438594000b1ca816f94f7573e5339eef46
DerToaster1138 <jadre03.2@gmail.com>
2024-06-11 12:40:04 +0200

    Cannibals

---
+++ Modul Algorithmen/MissionareKannibale/MissionareKannibale/MCState.cs
@@ -0,0 +1,47 @@
+﻿
+namespace MissionareKannibale
+{
+    internal class MCState
+    {
+        public int _maxNummer = 3;
+        public int _westM;
+        public int _westC;
+        public int _eastM;
+        public int _eastC;
+        public bool _boat;
+
+        public MCState(int missionaries, int cannibals, bool boat)
+        {
+            _westM = missionaries;
+            _westC = cannibals;
+            _eastM = _maxNummer - missionaries;
+            _eastC = _maxNummer - cannibals;
+            _boat = boat;
+        }
+
+        private bool GoalTest()
+        {
+            if (_eastC == _maxNummer && _eastM == _maxNummer && IsLegal())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsLegal()
+        {
+            if ((_westM < _westC && _westM > 0) || (_eastM < _eastC && _eastM > 0))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
+
---
+++ Modul Algorithmen/MissionareKannibale/MissionareKannibale/Program.cs
@@ -0,0 +1,81 @@
+﻿using MissionareKannibale;
+
+MCState currentState = new MCState(3, 3, true);
+
+void DisplaySolution(List<MCState> path)
+{
+	if (path.Count == 0)
+	{
+		return;
+	}
+	
+	var oldState = path[0];
+	Console.WriteLine(oldState);
+	 // todo - Ausgabe formatierne? 
+
+    for (int i = 1; i < path.Count; i++)
+    {
+		if (currentState._boat)
+		{
+			Console.WriteLine($"{oldState._eastM - currentState._eastM} Missionare und {oldState._eastC - currentState._eastC} Kannibalen vom Ostufer zum Westufer transportiert.");
+		}
+		else
+		{
+			Console.WriteLine($"{oldState._westM - currentState._westM} Missionare und {oldState._westC - currentState._westC} Kannibalen vom Westufer zum Ostufer transportiert.");
+		}
+		Console.WriteLine(currentState);
+		oldState = currentState;
+    }
+
+
+List<MCState> Successors()
+{
+    List<MCState> successors = [];
+	if (currentState._boat)
+	{
+		if (currentState._westM > 1)
+		{
+			successors.Add(new MCState(currentState._westM - 2,currentState._westC, !currentState._boat));
+		}
+		if (currentState._westM > 0)
+		{
+			successors.Add(new MCState(currentState._westM - 1, currentState._westC, !currentState._boat));
+		}
+		if (currentState._westC > 1)
+		{
+			successors.Add(new MCState(currentState._westM, currentState._westC - 2, !currentState._boat));
+		}
+		if (currentState._westC > 0)
+		{
+			successors.Add(new MCState(currentState._westM, currentState._westC - 1, !currentState._boat));
+		}
+		if (currentState._westC > 0 && currentState._westM > 0)
+		{
+			successors.Add(new MCState(currentState._westM - 1, currentState._westC - 1, !currentState._boat));
+		}
+	}
+	else
+	{
+        if (currentState._eastM > 1)
+        {
+            successors.Add(new MCState(currentState._westM + 2, currentState._westC, !currentState._boat));
+        }
+        if (currentState._eastM > 0)
+        {
+            successors.Add(new MCState(currentState._westM + 1, currentState._westC, !currentState._boat));
+        }
+        if (currentState._eastC > 1)
+        {
+            successors.Add(new MCState(currentState._westM, currentState._westC + 2, !currentState._boat));
+        }
+        if (currentState._eastC > 0)
+        {
+            successors.Add(new MCState(currentState._westM, currentState._westC + 1, !currentState._boat));
+        }
+        if (currentState._eastC > 0 && currentState._eastM > 0)
+        {
+            successors.Add(new MCState(currentState._westM + 1, currentState._westC + 1, !currentState._boat));
+        }
+    }
+    return successors.Where(x => x.IsLegal()).ToList();
+}
